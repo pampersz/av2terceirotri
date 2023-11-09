541cf0358ec0bb034e108b7613961c987e98e0f6
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2023-11-09 08:34:49 -0300

    som

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ while(contador<listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
     const idElementoAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
-        tocaSom("#som_tecla_aplausos");
         tocaSom(idAudio);
     }
     contador = contador + 1;
     console.log(contador);
-    
+
 }
