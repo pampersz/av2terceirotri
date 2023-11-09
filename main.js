8ccde9ffe41dc1db3721a84beae85fe6abbf74ce
Pamela Almeida <110232767+pampersz@users.noreply.github.com>
2023-11-09 08:17:38 -0300

    kn

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("ninguém precisa de padrão cada um com a sua beleza natural");
+function tocaSom(isElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idElementoAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom("#som_tecla_aplausos");
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    console.log(contador);
+    
+}
